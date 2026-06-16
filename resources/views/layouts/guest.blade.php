059614a10c205052648951b9e7d5736b23d156a2
blackcodeur2 <blackcodeurtest@gmail.com>
2026-06-16 11:41:53 +0100

    feat: integrate PWA configuration and implement guest layout template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#0A1628">
+        @laravelPWA
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
